776b93fa8b4edaea05e097d691ab8b6ef1003989
denisov-v <denisov-v@users.noreply.github.com>
2014-11-20 22:51:34 +0400

    command line argument support

--- MS_DRBG/program.cpp
+++ MS_DRBG/program.cpp
@@ -6,10 +6,19 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	if(argc < 2)
+	{
+		string exec_path(argv[0]);
+		string exec_name = exec_path.substr(exec_path.rfind('\\') + 1);
+		cout << "Usage: " << exec_name << " " << "<number_of_bytes>" << endl;
+		return 1;
+	}
+
+	size_t random_size = stoul(argv[1], nullptr, 10);
+
 	MsDrbg gen;
 	gen.instantiate(112, 7, 0, "Micali-Schnorr DRBG Personalization String");
 
-	size_t random_size = 500000000;
 	unsigned char* random = nullptr;
 	gen.generate_parallel(112, random_size, random, 3);
 		
@@ -17,7 +26,7 @@ int main(int argc, char* argv[])
 	size_t last_backslash = program_full_name.find_last_of("\\");
 	string program_path = program_full_name.substr(0,last_backslash+1);
 
-	ofstream file(program_path + "random.bin", ios::binary);
+	ofstream file(program_path + "msrnd.dat", ios::binary);
 	file.write(reinterpret_cast<char*>(random), random_size);
 
 	delete[] random;
