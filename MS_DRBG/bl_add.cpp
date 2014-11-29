df003b371f6325bb690ced5ba34a1c1085b0ad69
denisov-v <denisov-v@users.noreply.github.com>
2014-11-30 01:18:32 +0400

    biglong += fixed

--- MS_DRBG/bl_add.cpp
+++ MS_DRBG/bl_add.cpp
@@ -50,27 +50,4 @@ namespace blong
 		biglong right_long = biglong(right_op);
 		return (*this)+right_long;
 	}
-
-	biglong& biglong::operator+=(const biglong& right_op)
-	{
-		size_t max_size = value.size() > right_op.value.size() ? value.size() : right_op.value.size();
-		value.resize(max_size, 0);
-	
-		UNSIGINT carry_prev = 0;
-		for (size_t i = 0; i < value.size(); ++i)
-		{
-			value[i] = (i < value.size() ? value[i] : 0) + 
-				(i < right_op.value.size() ? right_op.value[i] : 0) + carry_prev;
-
-			if (value[i] >= BASE)
-			{
-				carry_prev = value[i] >> BASE_POWER;
-				value[i] &= REM_MASK;	
-			}
-		}
-		if(carry_prev!=0)
-			value.push_back(carry_prev);
-
-		return *this;
-	}
 }
